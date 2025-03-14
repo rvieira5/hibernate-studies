b8ff3123d31d5d5a233429ad685cd538f0c6a184
rvieira5 <r_vieira5@hotmail.com>
2025-03-14 07:54:30 +0000

    Configuring formatting

--- src/main/java/com/andcelsode/App.java
+++ src/main/java/com/andcelsode/App.java
@@ -1,9 +1,7 @@
 package com.andcelsode;
 
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
